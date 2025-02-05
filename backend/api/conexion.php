8cbe6f8025d7ea8a002a73c731f83fc7adc42a69
CristianPalacios20 <palaciosstiven208@gmail.com>
2025-02-04 20:00:12 -0500

    git commit -m "Probando conexion con vercel"

--- backend/api/conexion.php
+++ backend/api/conexion.php
@@ -1,21 +1,21 @@
-    <?php
-    // api/conexion.php
-    $servername = "localhost";
-    $username = "root";
-    $password = "2008";
-    $dbname = "portafolio_csp";
+<?php
+// api/conexion.php
+$servername = getenv('DB_HOST');  // Dirección IP o nombre de host de la base de datos
+$username = getenv('DB_USERNAME');  // Usuario de la base de datos
+$password = getenv('DB_PASS');  // Contraseña del usuario de la base de datos
+$dbname = getenv('DB_NAME');  // Nombre de la base de datos
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+// Conexión a la base de datos
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // if ($conn->connect_error) {
-    //     die("Conexión fallida: " . $conn->connect_error);
-    // }
-    if ($conn->connect_error) {
-        echo json_encode([
-            'success' => false,
-            'message' => 'Error en la conexión a la base de datos'
-        ]);
-        exit;
-    }
+if ($conn->connect_error) {
+    echo json_encode([
+        'success' => false,
+        'message' => 'Error en la conexión a MySQL en Google Cloud'
+    ]);
+} else {
+    echo 'Conexión exitosa a MySQL en Google Cloud';
+}
 
-    ?>
+?>
+
